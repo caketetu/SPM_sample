da9c0b2338678f7fcdc209cc93535333d361ec78
t-fukumoto <t-fukumoto@npm.co.jp>
2025-09-11 14:02:46 +0900

    fix

--- SPM_sample.ino
+++ SPM_sample.ino
@@ -1,7 +1,7 @@
 #include "MyModbus.h"
 #define DEV_ID 0x30
 
-UART Serial2(A4, A5);
+//UART Serial2(A4, A5);
 MyModbus modbus(DEV_ID);
 
 uint8_t coils[8];
@@ -10,7 +10,7 @@ int16_t regs[32];
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial2.begin(115200);
+  //Serial2.begin(115200);
   delay(2000);
 
   //各レジスタ初期化
@@ -27,52 +27,56 @@ void setup() {
   regs[1] = 0001; //バージョン
   regs[2] = 0000; //サブバージョン
 
-  Serial.println("Start...");
+  //Serial2.println("Start...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial2.available()) {
+  if (Serial.available()) {
     delay(10);
     uint8_t rbuf[50];
     int len = 0;
-    while (Serial2.available()) {
-      rbuf[len] = Serial2.read();
+    while (Serial.available()) {
+      rbuf[len] = Serial.read();
       len++;
     }
     //----デバック用----
-    Serial.print("Rx ");
+    /*
+    Serial2.print("Rx ");
     for (int i = 0; i < len; i++) {
-      Serial.print(rbuf[i], HEX);
-      Serial.print(' ');
+      Serial2.print(rbuf[i], HEX);
+      Serial2.print(' ');
     }
-    Serial.print("\r\n");
+    Serial2.print("\r\n");
+    */
 
     //modbus_taskでデータ処理
     uint8_t sbuf[50];
     int sl = modbus.modbus_task(rbuf, len, sbuf);
-    Serial2.write(sbuf, sl);
+    Serial.write(sbuf, sl);
 
     //----デバック用----
-    Serial.print("Tx ");
+    /*
+    Serial2.print("Tx ");
     for (int i = 0; i < sl; i++) {
-      Serial.print(sbuf[i], HEX);
-      Serial.print(' ');
+      Serial2.print(sbuf[i], HEX);
+      Serial2.print(' ');
     }
-    Serial.print("\r\n");
+    Serial2.print("\r\n");
     //------------------
 
-    Serial.print("coils ");
+    Serial2.print("coils ");
     for (int i = 0; i < 8; i++) {
-      Serial.print(*modbus.p_coils[i], HEX);
-      Serial.print(' ');
+      Serial2.print(*modbus.p_coils[i], HEX);
+      Serial2.print(' ');
     }
-    Serial.println(' ');
-    Serial.print("regs ");
+    Serial2.println(' ');
+    Serial2.print("regs ");
     for (int i = 0; i < 32; i++) {
-      Serial.print(*modbus.p_holding_regs[i], DEC);
-      Serial.print(' ');
+      Serial2.print(*modbus.p_holding_regs[i], DEC);
+      Serial2.print(' ');
     }
-    Serial.print("\r\n");
+    Serial2.print("\r\n");
+    */
   }
 }
