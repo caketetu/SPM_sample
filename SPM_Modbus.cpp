993809620a478052c1ed12018aca310e0c5d9bf2
t-fukumoto <t-fukumoto@npm.co.jp>
2025-10-08 11:24:01 +0900

    Modbusライブラリのクラスをやめて名前を変更した

--- SPM_Modbus.cpp
+++ SPM_Modbus.cpp
@@ -1,9 +1,24 @@
-// Modbus通信対応のスレーブデバイスのサンプル
-#include "MyModbus.h"
+/*
+ * Mymodbus.cpp
+ *
+ *  Created on: Sep 16, 2025
+ *      Author: t-fukumoto
+ */
 
-#define UNKOWN_FUNC 1
-#define INVAILED_ADR 2
-#define INVAILED_DATA 3
+#include "SPM_Modbus.h"
+
+String params = "/params.txt";
+uint8_t p_data[HOLDING_REGS_MAX*2];
+
+uint8_t modbus_id;
+int16_t *p_input_regs[INPUT_REGS_MAX];
+uint8_t *p_coils[COILS_MAX/8];
+int16_t *p_holding_regs[HOLDING_REGS_MAX];
+sCycFunc cycfunc0;
+sCycFunc cycfunc1;
+sCycFunc cycfunc2;
+sCycFunc cycfunc3;
+sCycFunc cycfunc4;
 
 // buf		受信データ
 // length	受信データ長(CRCを除く)
@@ -34,6 +49,46 @@ int16_t convert_int16(uint8_t h_data, uint8_t l_data) {
   return conv_data;
 }
 
+//パラメータセーブ
+int param_save(fs::FS &fs)
+{
+  fs.remove(params);
+
+  File file = fs.open(params,"w");
+  if( file ){
+    for(int i=0; i<HOLDING_REGS_MAX*2; i+=2){
+      p_data[i] = (uint8_t)(*p_holding_regs[i/2] >> 8);
+      p_data[i+1] = *p_holding_regs[i/2];
+    }
+
+    file.write((uint8_t*)p_data, sizeof(p_data));
+    file.close();
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+//パラメータロード
+int param_load(fs::FS &fs)
+{
+  File file = fs.open(params,"r"); //データを読み込む
+
+  if(file){ //ファイルを開いて以下を処理
+    file.read((uint8_t*)p_data, sizeof(p_data));
+    file.close(); //ファイルを閉じる
+    Serial.println("param load");
+    for(int i=0; i<HOLDING_REGS_MAX*2; i+=2){
+      *p_holding_regs[i/2] = convert_int16(p_data[i], p_data[i+1]);
+    }
+    return 1;
+  }
+  else{ //ファイルが開けなかった場合
+    return 0;
+  }
+}
+
 void make_exception_resp(uint8_t *buf, uint8_t id, uint8_t fc, uint8_t ec) {
   buf[0] = id;
   buf[1] = fc | 0x80;
@@ -44,13 +99,20 @@ void make_exception_resp(uint8_t *buf, uint8_t id, uint8_t fc, uint8_t ec) {
   return;
 }
 
-MyModbus::MyModbus(uint8_t dev_id) {
-  id = dev_id;
-  uint16_t i = 0;
+void SPM_ModbusSetAddress(uint8_t dev_id) {
+	modbus_id = dev_id;
 }
 
-int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
-  if (rbuf[0] == id) {
+int SPM_ModbusParamLoad(void) {
+  if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+    Serial.println("SPIFFS Mount Failed");
+    while(1);
+  }
+  return param_load(SPIFFS);
+}
+
+int SPM_ModbusTask(uint8_t *rbuf, int rl, uint8_t *sbuf) {
+  if (rbuf[0] == modbus_id) {
     uint16_t crc_check = convert_uint16(rbuf[rl - 1], rbuf[rl - 2]);
     if (crc_check == calc_crc(rbuf, rl - 2)) {
       switch (rbuf[1]) {
@@ -62,12 +124,12 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             uint16_t adr = convert_uint16(rbuf[2], rbuf[3]);
             uint16_t data_len = convert_uint16(rbuf[4], rbuf[5]);
             //レジスタ不正エラー
-            if (adr + data_len > REGS_MAX) {
-              make_exception_resp(sbuf, id, rbuf[1], INVAILED_ADR);
+            if (adr + data_len > HOLDING_REGS_MAX) {
+              make_exception_resp(sbuf, modbus_id, rbuf[1], INVAILED_ADR);
               return 5;
             }
             int sbuf_len = 0;
-            sbuf[0] = id;
+            sbuf[0] = modbus_id;
             sbuf[1] = rbuf[1];
             sbuf[2] = data_len * 2;
             sbuf_len = 3;
@@ -91,12 +153,12 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             uint16_t adr = convert_uint16(rbuf[2], rbuf[3]);
             uint16_t data_len = convert_uint16(rbuf[4], rbuf[5]);
             //レジスタ不正エラー
-            if (adr + data_len > REGS_MAX) {
-              make_exception_resp(sbuf, id, rbuf[1], INVAILED_ADR);
+            if (adr + data_len > INPUT_REGS_MAX) {
+              make_exception_resp(sbuf, modbus_id, rbuf[1], INVAILED_ADR);
               return 5;
             }
             int sbuf_len = 0;
-            sbuf[0] = id;
+            sbuf[0] = modbus_id;
             sbuf[1] = rbuf[1];
             sbuf[2] = data_len * 2;
             sbuf_len = 3;
@@ -124,7 +186,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
 
             //レジスタ不正エラー
             if (adr > COILS_MAX * 16) {
-              make_exception_resp(sbuf, id, rbuf[1], INVAILED_ADR);
+              make_exception_resp(sbuf, modbus_id, rbuf[1], INVAILED_ADR);
               return 5;
             }
 
@@ -135,10 +197,10 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
               *p_coils[index] = *p_coils[index] & ~coils_value;
             } else {
               //データ不正エラー
-              make_exception_resp(sbuf, id, rbuf[1], INVAILED_DATA);
+              make_exception_resp(sbuf, modbus_id, rbuf[1], INVAILED_DATA);
               return 5;
             }
-            sbuf[0] = id;
+            sbuf[0] = modbus_id;
             sbuf[1] = rbuf[1];
             sbuf[2] = rbuf[2];
             sbuf[3] = rbuf[3];
@@ -158,12 +220,12 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
           {
             uint16_t adr = convert_uint16(rbuf[2], rbuf[3]);
             //レジスタ不正エラー
-            if (adr > REGS_MAX) {
-              make_exception_resp(sbuf, id, rbuf[1], INVAILED_ADR);
+            if (adr > HOLDING_REGS_MAX) {
+              make_exception_resp(sbuf, modbus_id, rbuf[1], INVAILED_ADR);
               return 5;
             }
             *p_holding_regs[adr] = convert_int16(rbuf[4], rbuf[5]);
-            sbuf[0] = id;
+            sbuf[0] = modbus_id;
             sbuf[1] = rbuf[1];
             sbuf[2] = rbuf[2];
             sbuf[3] = rbuf[3];
@@ -185,15 +247,15 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             uint16_t data_len = convert_uint16(rbuf[4], rbuf[5]);
             uint16_t n_byte = rbuf[6];
             //レジスタ不正エラー
-            if (adr + data_len > REGS_MAX) {
-              make_exception_resp(sbuf, id, rbuf[1], INVAILED_ADR);
+            if (adr + data_len > HOLDING_REGS_MAX) {
+              make_exception_resp(sbuf, modbus_id, rbuf[1], INVAILED_ADR);
               return 5;
             }
 
             for (int i = 7; i < n_byte + 7; i += 2) {
               *p_holding_regs[adr++] = convert_int16(rbuf[i], rbuf[i + 1]);
             }
-            sbuf[0] = id;
+            sbuf[0] = modbus_id;
             sbuf[1] = rbuf[1];
             sbuf[2] = rbuf[2];
             sbuf[3] = rbuf[3];
@@ -206,7 +268,8 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
           }
           break;
 
-        //Cyclic Function 0
+        //ここから独自ファンクションコード。
+        //Cyclic Function 0(65)
         case 65:
           {
             sCycFunc cf = cycfunc0;
@@ -216,7 +279,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             }
 
             rc = 0;
-            sbuf[rc++] = id;
+            sbuf[rc++] = modbus_id;
             sbuf[rc++] = rbuf[1];
             for (int i = 0; i < cf.rx_len; i++) {
               sbuf[rc++] = (uint8_t)(*cf.rx_adr[i] >> 8);
@@ -228,7 +291,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             return rc;
           }
 
-        //Cyclic Function 1
+        //Cyclic Function 1(66)
         case 66:
           {
             sCycFunc cf = cycfunc1;
@@ -238,7 +301,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             }
 
             rc = 0;
-            sbuf[rc++] = id;
+            sbuf[rc++] = modbus_id;
             sbuf[rc++] = rbuf[1];
             for (int i = 0; i < cf.rx_len; i++) {
               sbuf[rc++] = (uint8_t)(*cf.rx_adr[i] >> 8);
@@ -250,7 +313,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             return rc;
           }
 
-        //Cyclic Function 2
+        //Cyclic Function 2(67)
         case 67:
           {
             sCycFunc cf = cycfunc2;
@@ -260,7 +323,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             }
 
             rc = 0;
-            sbuf[rc++] = id;
+            sbuf[rc++] = modbus_id;
             sbuf[rc++] = rbuf[1];
             for (int i = 0; i < cf.rx_len; i++) {
               sbuf[rc++] = (uint8_t)(*cf.rx_adr[i] >> 8);
@@ -272,7 +335,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             return rc;
           }
 
-        //Cyclic Function 3
+        //Cyclic Function 3(68)
         case 68:
           {
             sCycFunc cf = cycfunc3;
@@ -282,7 +345,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             }
 
             rc = 0;
-            sbuf[rc++] = id;
+            sbuf[rc++] = modbus_id;
             sbuf[rc++] = rbuf[1];
             for (int i = 0; i < cf.rx_len; i++) {
               sbuf[rc++] = (uint8_t)(*cf.rx_adr[i] >> 8);
@@ -294,7 +357,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             return rc;
           }
 
-        //Cyclic Function 4
+        //Cyclic Function 4(69)
         case 69:
           {
             sCycFunc cf = cycfunc4;
@@ -304,7 +367,7 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             }
 
             rc = 0;
-            sbuf[rc++] = id;
+            sbuf[rc++] = modbus_id;
             sbuf[rc++] = rbuf[1];
             for (int i = 0; i < cf.rx_len; i++) {
               sbuf[rc++] = (uint8_t)(*cf.rx_adr[i] >> 8);
@@ -316,8 +379,31 @@ int MyModbus::modbus_task(uint8_t *rbuf, int rl, uint8_t *sbuf) {
             return rc;
           }
 
+        //パラメータセーブ
+        case 75:
+          {
+            int rc=0;
+            sbuf[rc++] = modbus_id;
+            sbuf[rc++] = rbuf[1];
+            if(param_save(SPIFFS)){
+              sbuf[rc++] = 0;
+              sbuf[rc++] = 1;
+              uint16_t crc = calc_crc(sbuf, rc);
+              sbuf[rc++] = crc;
+              sbuf[rc++] = (uint8_t)(crc >> 8);
+            }else{
+              sbuf[rc++] = 0;
+              sbuf[rc++] = 0;
+              uint16_t crc = calc_crc(sbuf, rc);
+              sbuf[rc++] = crc;
+              sbuf[rc++] = (uint8_t)(crc >> 8);
+            }
+            return rc;
+          }
+
+        //default UNKNOWN FUNCTIONエラー
         default:
-          make_exception_resp(sbuf, id, rbuf[1], UNKOWN_FUNC);
+          make_exception_resp(sbuf, modbus_id, rbuf[1], UNKOWN_FUNC);
           return 5;
           break;
       }
---
+++ SPM_Modbus.h
@@ -0,0 +1,50 @@
+/*
+ * Mymodbus.h
+ *
+ *  Created on: Sep 16, 2025
+ *      Author: t-fukumoto
+ */
+
+#ifndef __SPM_MODBUS_H_
+#define __SPM_MODBUS_H_
+
+//Arduino IDEの時は必要
+#include <Arduino.h>
+#include "FS.h"
+#include "SPIFFS.h"
+
+#define UNKOWN_FUNC 1
+#define INVAILED_ADR 2
+#define INVAILED_DATA 3
+
+//Arduino IDEの時は不要
+// #define	int16_t		signed short
+// #define	uint16_t	unsigned short
+// #define	uint8_t		unsigned char
+
+#define INPUT_REGS_MAX 32
+#define HOLDING_REGS_MAX 16
+#define COILS_MAX 16
+#define FORMAT_SPIFFS_IF_FAILED true
+
+typedef struct{
+    int16_t tx_len;
+    int16_t rx_len;
+    int16_t *tx_adr[8];
+    int16_t *rx_adr[8];
+}sCycFunc;
+
+extern int16_t *p_input_regs[INPUT_REGS_MAX];
+extern uint8_t *p_coils[COILS_MAX/8];
+extern int16_t *p_holding_regs[HOLDING_REGS_MAX];
+extern sCycFunc cycfunc0;
+extern sCycFunc cycfunc1;
+extern sCycFunc cycfunc2;
+extern sCycFunc cycfunc3;
+extern sCycFunc cycfunc4;
+
+void SPM_ModbusSetAddress(uint8_t dev_adr);
+int SPM_ModbusTask(uint8_t *rbuf, int rl, uint8_t *s_buf);
+int SPM_ModbusParamLoad(void);
+
+#endif /* __SPM_MODBUS_H_ */
