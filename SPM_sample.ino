4034b5c55d25698a58d9f9d3b634cae6f9a07d99
TP <tp@example.com>
2025-10-23 10:28:04 +0900

    fix

--- SPM_sample.ino
+++ SPM_sample.ino
@@ -1,12 +1,11 @@
-#include "MyModbus.h"
+#include "SPM_Modbus.h"
 #define DEV_ID 0x30
 
 //UART Serial2(A4, A5);
-MyModbus modbus(DEV_ID);
 
 int16_t ro_value[16];
 int16_t rw_value[16];
-uint8_t dio=0;
+uint8_t dio = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,74 +13,77 @@ void setup() {
   //Serial2.begin(115200);
   delay(2000);
 
-  ro_value[0] = 48;   //モデルナンバー
-  ro_value[1] = 0002; //バージョン
-  ro_value[2] = 0000; //サブバージョン
+  SPM_ModbusSetAddress(DEV_ID);
+
+  ro_value[0] = 48;    //モデルナンバー
+  ro_value[1] = 0002;  //バージョン
+  ro_value[2] = 0000;  //サブバージョン
 
   //各レジスタ初期化
   //コイル---rwレジスタと同じ
-  modbus.p_coils[0] = &dio;//&rw_value[0];
-  modbus.p_coils[1] = &dio;//&rw_value[1];
+  p_coils[0] = &dio;  //&rw_value[0];
+  p_coils[1] = &dio;  //&rw_value[1];
 
-  for(int i=0; i<16; i++){
-    modbus.p_holding_regs[i] = &rw_value[i];
-    modbus.p_input_regs[i] = &ro_value[i];
+  for (int i = 0; i < 16; i++) {
+    p_holding_regs[i] = &rw_value[i];
+    p_input_regs[i] = &ro_value[i];
   }
-  for(int i=16; i<32; i++){
-    modbus.p_input_regs[i] = &rw_value[i-16];
+  for (int i = 16; i < 32; i++) {
+    p_input_regs[i] = &rw_value[i - 16];
   }
 
   //Cyclic Function0登録
-  modbus.cycfunc0.rx_len = 1;
-  modbus.cycfunc0.rx_adr[0] = &rw_value[0];
-  modbus.cycfunc0.tx_len = 1;
-  modbus.cycfunc0.tx_adr[0] = &rw_value[0];
+  cycfunc0.rx_len = 1;
+  cycfunc0.rx_adr[0] = &rw_value[0];
+  cycfunc0.tx_len = 1;
+  cycfunc0.tx_adr[0] = &rw_value[0];
 
   //Cyclic Function1登録
-  modbus.cycfunc1.rx_len = 2;
-  modbus.cycfunc1.rx_adr[0] = &rw_value[0];
-  modbus.cycfunc1.rx_adr[1] = &rw_value[1];
-  modbus.cycfunc1.tx_len = 2;
-  modbus.cycfunc1.tx_adr[0] = &rw_value[0];
-  modbus.cycfunc1.tx_adr[1] = &rw_value[1];
+  cycfunc1.rx_len = 2;
+  cycfunc1.rx_adr[0] = &rw_value[0];
+  cycfunc1.rx_adr[1] = &rw_value[1];
+  cycfunc1.tx_len = 2;
+  cycfunc1.tx_adr[0] = &rw_value[0];
+  cycfunc1.tx_adr[1] = &rw_value[1];
 
   //Cyclic Function2登録
-  modbus.cycfunc2.rx_len = 3;
-  modbus.cycfunc2.rx_adr[0] = &rw_value[0];
-  modbus.cycfunc2.rx_adr[1] = &rw_value[1];
-  modbus.cycfunc2.rx_adr[2] = &rw_value[2];
-  modbus.cycfunc2.tx_len = 3;
-  modbus.cycfunc2.tx_adr[0] = &rw_value[0];
-  modbus.cycfunc2.tx_adr[1] = &rw_value[1];
-  modbus.cycfunc2.tx_adr[2] = &rw_value[2];
+  cycfunc2.rx_len = 3;
+  cycfunc2.rx_adr[0] = &rw_value[0];
+  cycfunc2.rx_adr[1] = &rw_value[1];
+  cycfunc2.rx_adr[2] = &rw_value[2];
+  cycfunc2.tx_len = 3;
+  cycfunc2.tx_adr[0] = &rw_value[0];
+  cycfunc2.tx_adr[1] = &rw_value[1];
+  cycfunc2.tx_adr[2] = &rw_value[2];
 
   //Cyclic Function3登録
-  modbus.cycfunc3.rx_len = 1;
-  modbus.cycfunc3.rx_adr[0] = &rw_value[0];
-  modbus.cycfunc3.tx_len = 2;
-  modbus.cycfunc3.tx_adr[0] = &rw_value[0];
-  modbus.cycfunc3.tx_adr[1] = &rw_value[1];
+  cycfunc3.rx_len = 1;
+  cycfunc3.rx_adr[0] = &rw_value[0];
+  cycfunc3.tx_len = 2;
+  cycfunc3.tx_adr[0] = &rw_value[0];
+  cycfunc3.tx_adr[1] = &rw_value[1];
 
   //Cyclic Function4登録
-  modbus.cycfunc4.rx_len = 8;
-  modbus.cycfunc4.rx_adr[0] = &rw_value[0];
-  modbus.cycfunc4.rx_adr[1] = &rw_value[1];
-  modbus.cycfunc4.rx_adr[2] = &rw_value[2];
-  modbus.cycfunc4.rx_adr[3] = &rw_value[3];
-  modbus.cycfunc4.rx_adr[4] = &rw_value[4];
-  modbus.cycfunc4.rx_adr[5] = &rw_value[5];
-  modbus.cycfunc4.rx_adr[6] = &rw_value[6];
-  modbus.cycfunc4.rx_adr[7] = &ro_value[7];
-  modbus.cycfunc4.tx_len = 8;
-  modbus.cycfunc4.tx_adr[0] = &rw_value[0];
-  modbus.cycfunc4.tx_adr[1] = &rw_value[1];
-  modbus.cycfunc4.tx_adr[2] = &rw_value[2];
-  modbus.cycfunc4.tx_adr[3] = &rw_value[3];
-  modbus.cycfunc4.tx_adr[4] = &rw_value[4];
-  modbus.cycfunc4.tx_adr[5] = &rw_value[5];
-  modbus.cycfunc4.tx_adr[6] = &rw_value[6];
-  modbus.cycfunc4.tx_adr[7] = &rw_value[7];
+  cycfunc4.rx_len = 8;
+  cycfunc4.rx_adr[0] = &rw_value[0];
+  cycfunc4.rx_adr[1] = &rw_value[1];
+  cycfunc4.rx_adr[2] = &rw_value[2];
+  cycfunc4.rx_adr[3] = &rw_value[3];
+  cycfunc4.rx_adr[4] = &rw_value[4];
+  cycfunc4.rx_adr[5] = &rw_value[5];
+  cycfunc4.rx_adr[6] = &rw_value[6];
+  cycfunc4.rx_adr[7] = &ro_value[7];
+  cycfunc4.tx_len = 8;
+  cycfunc4.tx_adr[0] = &rw_value[0];
+  cycfunc4.tx_adr[1] = &rw_value[1];
+  cycfunc4.tx_adr[2] = &rw_value[2];
+  cycfunc4.tx_adr[3] = &rw_value[3];
+  cycfunc4.tx_adr[4] = &rw_value[4];
+  cycfunc4.tx_adr[5] = &rw_value[5];
+  cycfunc4.tx_adr[6] = &rw_value[6];
+  cycfunc4.tx_adr[7] = &rw_value[7];
 
+  int t = SPM_ModbusParamLoad();
   //Serial2.println("Start...");
 }
 
@@ -107,7 +109,7 @@ void loop() {
 
     //modbus_taskでデータ処理
     uint8_t sbuf[100];
-    int sl = modbus.modbus_task(rbuf, len, sbuf);
+    int sl = SPM_ModbusTask(rbuf, len, sbuf);
     Serial.write(sbuf, sl);
 
     //----デバック用----
@@ -122,13 +124,13 @@ void loop() {
 
     Serial2.print("coils ");
     for (int i = 0; i < 8; i++) {
-      Serial2.print(*modbus.p_coils[i], HEX);
+      Serial2.print(*p_coils[i], HEX);
       Serial2.print(' ');
     }
     Serial2.println(' ');
     Serial2.print("regs ");
     for (int i = 0; i < 32; i++) {
-      Serial2.print(*modbus.p_holding_regs[i], DEC);
+      Serial2.print(*p_holding_regs[i], DEC);
       Serial2.print(' ');
     }
     Serial2.print("\r\n");
